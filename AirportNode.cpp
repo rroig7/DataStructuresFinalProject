e4a9ebb5ae789405c722c306ce1c4e8ba96f8c13
Mangled-Night <148656152+Mangled-Night@users.noreply.github.com>
2024-04-18 22:57:36 -0400

    Tested Hash Table Implementation Along with Adjacency List in main.cpp

--- AirportNode.cpp
+++ AirportNode.cpp
@@ -8,8 +8,13 @@ AirportNode::AirportNode(std::string Airport) {
     name = Airport;
 }
 
-void AirportNode::Add_Edge( AirportNode* OtherNode, int Distance, int Cost){ 
-    Edges.push_back(new DirectFlight<AirportNode*>(&OtherNode, Distance, Cost) );
+void AirportNode::Add_Edge( AirportNode* OtherNode, int Distance, int Cost){
+    Edges.push_back(new DirectFlight(OtherNode, Distance, Cost) );
 }
 
-
+void AirportNode::Print_Edges() {
+    for(int i = 0; i < Edges.size(); i++){
+        cout<< Edges[i]->Port->name<<" ";
+    }
+}
+  
